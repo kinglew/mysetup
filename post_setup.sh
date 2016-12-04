c3047fbd2a41f184bb0cd69cf3a32041b944d005
King Lew <king.lew@outlook.com>
2016-12-04 15:53:09 -0500

    fix post_setup

--- post_setup.sh
+++ post_setup.sh
@@ -1,6 +1,7 @@
 #source ~/.bashrc
 #shell needs to be reloaded before executing this
 
+echo "Verifying git configs..."
 if [[ $(uname) =~ ^CYGWIN ]];
 then
   git config --global credential.helper store ;
@@ -9,12 +10,14 @@ else
 fi
 
 ######################################################
-# Install npm packages
+# Install/Update npm packages
 ######################################################
-npm install -g yo gulp karma protractor bower jshint nodemon generator-gulp-angular angular-cli react-native-cli typings
+echo "Installing/Updating npm packages..."
+npm update -g yo gulp karma protractor jshint nodemon generator-gulp-angular angular-cli react-native-cli typings
 
 ######################################################
 # Update zsh aliases
 ######################################################
+echo "Update zsh configs/aliases..."
 chmod +x update_zsh.sh
 ./update_zsh.sh
