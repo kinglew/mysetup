1605c9433c8382c86a9c8b97a3861a6557967dd8
King Lew <king.lew@outlook.com>
2017-10-16 20:44:15 -0400

    removed deprecated symlinks

--- dev_windows_setup.sh
+++ dev_windows_setup.sh
@@ -1,19 +1,6 @@
 ## pre-req: babun(w/ git) and chocolatey installed. this should be executed from babun (run as administrator)
 command -v choco2 >/dev/null 2>&1 || { echo >&2 "ERROR: chocolatey not installed"; exit 1; }
 
-echo "Create symbolic links" #$userprofile_path
-userprofile_path=$(printenv | grep USERPROFILE | sed s/USERPROFILE=//)
-mklink_cmd="mklink /j c:\kl_userprofile \"$userprofile_path\""
-cmd.exe /c "$mklink_cmd"
-
-echo "Adding embed.bat for babun embedded terminal mode"
-if ls /cygdrive/c/kl_userprofile/.babun/embed.bat 1> /dev/null 2>&1; then
-    echo "Babun embed.bat already exists. Ignore."
-else
-    cp ./windows/embed.bat /cygdrive/c/kl_userprofile/.babun/embed.bat
-    echo "Added .babun/embed.bat"
-fi
-
 ######################################################
 # Install/Upgrade apps using Chocolatey
 ######################################################
