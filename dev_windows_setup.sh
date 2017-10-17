5678e2ebfc7bedbe0f0968d422719ac25a60d04e
King Lew <king.lew@outlook.com>
2017-10-16 20:01:52 -0400

    updated list of apps to install

--- dev_windows_setup.sh
+++ dev_windows_setup.sh
@@ -21,34 +21,26 @@ fi
 echo "Installing/Upgrading applications using Chocolatey"
 choco upgrade winscp -y
 choco upgrade GoogleChrome -y
-choco upgrade fiddler4 -y
+choco upgrade vivaldi -y
 choco upgrade dropbox -y
-choco upgrade kdiff3 -y
 choco upgrade 7zip -y
 choco upgrade paint.net -y
 choco upgrade listary -y
 choco upgrade ccleaner -y
-choco upgrade keepass.upgrade -y
 choco upgrade conemu -y
 choco upgrade switcheroo -y
 choco upgrade procexp -y
 
 ## dev tools
-choco upgrade velocity -y
 choco upgrade boxstarter -y
 choco upgrade vagrant -y
+choco upgrade postman -y
 
 ## dev frameworks
-choco upgrade jdk8 -y
-#choco upgrade nodejs -y #use nodist/nvm instead
 choco upgrade nodist -y
-choco upgrade phantomjs -y
 choco upgrade python -y
-choco upgrade nuget.commandline -y
 choco upgrade golang -y
 
 ## dev IDEs
 choco upgrade visualstudiocode -y
-#choco upgrade webstorm -y
-#choco upgrade visualstudio2015community -y
 choco upgrade intellijidea-ultimate -y
